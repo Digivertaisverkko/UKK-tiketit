71f6efa68231034554fec816935b34b626fb168d
Henri K <12832691+nkahe@users.noreply.github.com>
2022-09-26 12:43:57 +0300

    ennen css->scss vaihtoa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
 import { UserManagementModule } from './user-management/user-management.module';
 import { TestingModule } from './testing/testing.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,31 @@
+// Material elements which are used in core app here.
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
+const materialModules = [
+  MatInputModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatCheckboxModule,
+  MatIconModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule, ...materialModules
+  ],
+  exports: [
+    materialModules
+  ]
+})
+export class MaterialModule { }
