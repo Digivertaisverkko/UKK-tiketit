d8db22772111d9c46103f7a2fdcf9f4e918b84c0
henri <12832691+nkahe@users.noreply.github.com>
2022-10-20 16:39:18 +0300

    header komponentti shared -> core -moduuliin

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,18 +4,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from '../shared/material.module';
+
 import { AuthService } from './auth.service';
 
+import { HeaderComponent } from '../core/header/header.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
+    MaterialModule,
   ],
   providers: [
     AuthService
   ],
   exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
