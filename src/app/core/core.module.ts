8e8c0ea2058dfd906ec76da47efeff1524e6de3d
henri <12832691+nkahe@users.noreply.github.com>
2022-10-21 14:26:49 +0300

    footerin koodia alustavasti. 'sisään kirjautumatta' toimimaan. authServicen getSessionID lisäys.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,25 +5,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../shared/material.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AuthService } from './auth.service';
 
 import { HeaderComponent } from '../core/header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     MaterialModule,
+    MatSidenavModule
   ],
   providers: [
     AuthService
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
