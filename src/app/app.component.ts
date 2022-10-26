22ef85343c5af4a079b882897944d76c3ae88053
henri <12832691+nkahe@users.noreply.github.com>
2022-10-26 10:02:47 +0300

    vanhoja koodeja pois

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from './core/auth.service';
 import { Router } from '@angular/router';
 import { BreakpointObserver } from '@angular/cdk/layout';
 import { Breakpoints } from '@angular/cdk/layout';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   public isPhonePortrait = false;
 
   constructor(
@@ -49,34 +48,6 @@ export class AppComponent implements OnInit, OnDestroy {
     } else {
       this.authService.isUserLoggedIn$.next(true);
     }
-  }
-
-  // public initializeApp-old() {
-  //   console.log('--- App.component Initialize ajettu ---');
-    
-  //   this.authService.onIsUserLoggedIn().subscribe(response => {
-  //     console.log('nIsUserLoggedIn saatiin dataa: ' + response);
-  //     console.log('Tsekataan, onko kirjautunut');
-  //     if (response == true) {
-  //       this.router.navigateByUrl('/ListingComponent', { replaceUrl: true });
-  //     } else {
-  //       console.log('ei ole kirjautunut');
-  //       /* Oma kirjautumistapa on oletus ennen kuin käyttäjä valitsee
-  //          Ei siirrytä suoraan /login, koska url sisältää loginid:n. */
-  //       this.authService.sendAskLoginRequest('own').then((response: string) => {
-  //         console.log('AppComponent: got url from server: ' + response);
-  //         if (response !== 'error') {
-  //           this.router.navigateByUrl(response);
-  //         }
-  //       }).catch (error => {
-  //         console.log('Error: Route for login not found: ' + error);
-  //       })
-  //     }
-  //   });
-  // }
-
-  ngOnDestroy(): void {
-    //  this.authService.unsubscribeIsUserLoggedin();
   }
 
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 // import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
-// import { Location } from '@angular/common';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
 
@@ -11,30 +10,13 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  // public loggedInSubscription: Subscription;
-  // public isUserLoggedIn: boolean = false;
   public isUserLoggedIn: Observable<boolean>;
-  // public message$ = new Subject<any>();
-
-  //constructor(location: Location, router: Router) {
 
   constructor(private authService: AuthService,
     private activatedRoute: ActivatedRoute,
     private router: Router) {
     this.isUserLoggedIn = this.authService.onIsUserLoggedIn();
-    // this.loggedInSubscription = this.authService.onIsUserLoggedIn().subscribe(loggedInState =>
-    //   this.isUserLoggedIn = loggedInState
-    //);
 
-    // Vaihtoehtoinen tapa sisäänkirjautumisen seurantaan, jossa seurataan router -tapahtumia.
-    // router.events.subscribe(route => {
-    //   if (location.path().includes('/login')) {
-    //     console.log(location.path());
-    //     this.isAccountMenuShown = false;
-    //   } else {
-    //     this.isAccountMenuShown = true;
-    //   }
-    // });
   }
 
   public logOut() {
