cede9cbfa6c604736d06ee593bbc7b9aa5dff5b4
Henri K <12832691+nkahe@users.noreply.github.com>
2022-11-07 17:29:04 +0200

    tikettien listauksen sorting -muutokset ja esimerkki-sivulle route

--- src/app/ticket/listing/listing.component.ts
+++ src/app/ticket/listing/listing.component.ts
@@ -12,8 +12,28 @@ export interface Question {
   id: number;
   otsikko: string;
   aikaleima: string;
-  aloittaja: number;
+  aloittaja: {
+    id: number,
+    nimi: string;
+    sposti: string;
+    asema: string;
+  };
 }
+
+export interface Sortable {
+  id: number;
+  otsikko: string;
+  aikaleima: string;
+  aloittajanNimi: string
+}
+
+
+// const DATA = [
+// { id: 3, otsikko: "”Index out of bounds”?", aikaleima: "2022-10-18T07:07:07.693Z", aloittaja: 3},
+// { id: 6, otsikko: "”} Expected”?", aikaleima: "2022-10-21T07:07:07.847Z", aloittaja: 2 },
+// { id: 7, otsikko: "Uusi tiketti", aikaleima: "2022-11-02T07:49:12.382Z", aloittaja: 1 }
+
+// ]
 
 @Component({
   selector: 'app-listing',
@@ -21,12 +41,11 @@ export interface Question {
   styleUrls: ['./listing.component.scss']
 })
 export class ListingComponent implements AfterViewInit, OnInit {
-
-  displayedColumns: string[] = ['otsikko', 'aikaleima', 'aloittaja'];
- ticketViewLink: string = environment.apiBaseUrl + '/ticket-view/';
-
   dataSource = new MatTableDataSource<Question>();
+  displayedColumns: string[] = ['otsikko', 'aikaleima', 'aloittaja.nimi'];
+  ticketViewLink: string = environment.apiBaseUrl + '/ticket-view/';
   userID = '3';
+  courseID: string = '1';
   // dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   //displayedColumns: string[] = ['id', 'nimi', 'ulkotunnus']
@@ -35,12 +54,18 @@ export class ListingComponent implements AfterViewInit, OnInit {
   constructor(private _liveAnnouncer: LiveAnnouncer,
     private router: Router,
     private ticket: TicketService) {
-      this.ticket.getQuestions('1').then( response => {
+      this.ticket.getQuestions(this.courseID).then( response => {
+
+        // let sortableData = response.map(x => {
+        //   x.aloittajaNimi = x.aloittaja.nimi;
+        // })
         // this.dataSource = new MatTableDataSource(response);
+
         this.dataSource = new MatTableDataSource<Question>(response);
         console.log('Saatiin vastaus (alla):');
         console.dir(response);
       });
+      // this.dataSource = new MatTableDataSource(DATA);
     }
 
   @ViewChild(MatSort)
--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -10,10 +10,13 @@ import { MatSortModule } from '@angular/material/sort';
 
 import { MatCardModule } from '@angular/material/card';
 
+import { EsimerkkiListingComponent } from './listing/esimerkki-listing.component';
+
 @NgModule({
   declarations: [
     SubmitTicketComponent,
-    TicketViewComponent
+    TicketViewComponent,
+    EsimerkkiListingComponent
   ],
   imports: [
     CommonModule,
