ecfc43453dcce6e465bafd12ebd00c5c0d39fa22
Erkki Tervo <erkki.tervo@oulu.fi>
2022-11-09 16:06:37 +0200

    Ticket-view: Paginator huvin ja urheilun vuoksi

--- src/app/ticket/ticket-view/ticket-view.component.ts
+++ src/app/ticket/ticket-view/ticket-view.component.ts
@@ -1,13 +1,19 @@
 import { ActivatedRoute } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { TicketService, Ticket, Tila } from '../ticket.service';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { TicketService, Ticket, Tila, Comment } from '../ticket.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-ticket-view',
   templateUrl: './ticket-view.component.html',
   styleUrls: ['./ticket-view.component.scss']
 })
-export class TicketViewComponent implements OnInit {
+export class TicketViewComponent implements OnInit, OnDestroy {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  commentObs: Observable<any> | undefined;
+  dataSource: MatTableDataSource<Comment> | undefined;
   ticket: Ticket;
   tila: typeof Tila;
   commentText: string;
@@ -16,7 +22,8 @@ export class TicketViewComponent implements OnInit {
 
   constructor(
     private ticketService: TicketService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private changeDetectorRef: ChangeDetectorRef
     ) {
       this.ticket = {} as Ticket;
       this.tila = Tila;
@@ -29,14 +36,32 @@ export class TicketViewComponent implements OnInit {
     this.ticketService.getTicketInfo(this.ticketID)
       .then(response => {
         this.ticket = response;
+        this.setPagination(response.kommentit);
         this.isLoaded = true;
       });
   }
 
+  ngOnDestroy() {
+    if (this.dataSource) {
+      this.dataSource.disconnect();
+    }
+  }
+
   public sendComment(): void {
     this.ticketService.addComment(this.ticketID, this.commentText)
-      .then(() => { this.ticketService.getTicketInfo(this.ticketID).then(response => { this.ticket = response }) })
+      .then(() => { this.ticketService.getTicketInfo(this.ticketID)
+        .then(response => {
+          this.ticket = response;
+          this.setPagination(response.kommentit);
+        }) })
       .then(() => { this.commentText = '' });
+  }
+
+  private setPagination(comments: Comment[]): void {
+    this.dataSource = new MatTableDataSource<Comment>(comments);
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.commentObs = this.dataSource.connect();
   }
 
 }
