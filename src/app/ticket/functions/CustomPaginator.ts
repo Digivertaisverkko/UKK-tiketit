442e184668727c342370d675cbba16d3b675b037
Henri K <12832691+nkahe@users.noreply.github.com>
2022-11-18 15:26:59 +0200

    paginatorin käännös toistaiseksi näin

--- src/app/ticket/functions/CustomPaginator.ts
+++ src/app/ticket/functions/CustomPaginator.ts
@@ -2,8 +2,15 @@ import { MatPaginatorIntl } from '@angular/material/paginator';
 
 export function CustomPaginator() {
   const customPaginatorIntl = new MatPaginatorIntl();
-
-  customPaginatorIntl.itemsPerPageLabel = $localize`:@@Kysymyksiä sivulla:Kysymyksiä sivulla`;
-
+  // FIXME: Miksi $localize ei toimi tässä funktiossa?
+  if (localStorage.getItem('language')?.substring(0,2) == 'fi') {
+    customPaginatorIntl.itemsPerPageLabel = "Kysymyksiä sivulla";
+    customPaginatorIntl.firstPageLabel = "Ensimmäinen sivu";
+    customPaginatorIntl.lastPageLabel  = "Viimeinen sivu";
+    customPaginatorIntl.nextPageLabel = "Seuraava sivu";
+    customPaginatorIntl.previousPageLabel = "Edellinen sivu";
+    customPaginatorIntl.getRangeLabel = (page, pageSize, length) =>
+      "Kysymykset " + String(page) + "–" + String(pageSize) + " / " + String(length);
+  }
   return customPaginatorIntl;
 }
