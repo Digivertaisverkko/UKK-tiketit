f542a4cc6fec6f51f91c8db9ea8d5ba5e3a27626
Henri K <12832691+nkahe@users.noreply.github.com>
2022-12-08 10:34:08 +0200

    loginin seuranta ja sinne ohjaus app.component -> auth.guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,20 +30,22 @@ export class AppComponent implements OnInit {
 
   public initializeApp() {
     this.authService.initialize();
-    this.authService.onIsUserLoggedIn().subscribe(response => {
-      /* Oma kirjautumistapa on oletus ennen kuin käyttäjä valitsee kirjautumisruudussa
-        jonkin muun tavan. Ei siirrytä suoraan /login, koska palvelimelta saatava
-        URL sisältää login id:n. */
-      if (response == false) {
-        this.authService.sendAskLoginRequest('own').then((response: string) => {
-          this.router.navigateByUrl(response);
-        }).catch(error => {
-          console.error(error.message);
-        })
-        // Ei saatu login id:ä, mutta näytetään kirjautumisruutu.
-        // this.router.navigateByUrl('login', { replaceUrl: true });
-      }
-    });
-  }
+    
+    // this.authService.onIsUserLoggedIn().subscribe(response => {
+    //   /* Oma kirjautumistapa on oletus ennen kuin käyttäjä valitsee kirjautumisruudussa
+    //     jonkin muun tavan. Ei siirrytä suoraan /login, koska palvelimelta saatava
+    //     URL sisältää login id:n. */
+    //   if (response == false) {
+    //     this.authService.sendAskLoginRequest('own').then((response: string) => {
+    //       this.router.navigateByUrl(response);
+    //     }).catch(error => {
+    //       console.error(error.message);
+    //     })
+    //     // Ei saatu login id:ä, mutta näytetään kirjautumisruutu.
+    //     // this.router.navigateByUrl('login', { replaceUrl: true });
+    //   }
+    // });
 
+
+  }
 }
--- src/app/user-management/auth.guard.ts
+++ src/app/user-management/auth.guard.ts
@@ -4,16 +4,29 @@ import { Router } from "@angular/router";
 
 import { AuthService } from "../core/auth.service";
 
-export const authGuard: CanActivateFn | CanActivateChildFn = () => {
-  const auth = inject(AuthService);
+export const authGuard: CanActivateFn = async () => {
+  const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (auth.getIsUserLoggedIn()) {
-    // console.log('authGuard: olet kirjautunut.');
+  if (authService.getIsUserLoggedIn()) {
+    console.log('authGuard: olet kirjautunut.');
     return true
   } else {
     // console.log('authGuard: et ole kirjautunut.');
+    // authService.onIsUserLoggedIn().subscribe(response => {
+      /* Oma kirjautumistapa on oletus ennen kuin käyttäjä valitsee kirjautumisruudussa
+        jonkin muun tavan. Ei siirrytä suoraan /login, koska palvelimelta saatava
+        URL sisältää login id:n. */
+      // if (response == false) {
+        const loginUrl = await authService.sendAskLoginRequest('own');
+        console.log('authGuard: loginUrl: ' + loginUrl)
+        return router.navigateByUrl(loginUrl);
+        // Ei saatu login id:ä, mutta näytetään kirjautumisruutu.
+        // this.router.navigateByUrl('login', { replaceUrl: true });
+      // } else {
+      //   return true
+      // }
+    // });
+  
   }
-
-  return router.parseUrl('/login');
 }
