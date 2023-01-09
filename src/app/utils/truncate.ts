423611af428310a526f90a3bc672a29905d30884
Henri K <12832691+nkahe@users.noreply.github.com>
2023-01-09 12:04:37 +0200

    tekstin lyhennysfunktio toimimaan, vaikka ei saisi stringiä

--- src/app/utils/truncate.ts
+++ src/app/utils/truncate.ts
@@ -1,5 +1,8 @@
 // Lyhennä pitkiä merkkijonoja vaihtoehtoisesti sanaraja huomioiden.
 export function truncate( str: string, length: number, useWordBoundary: boolean ){
+  if (typeof str !== 'string') {
+    return str;
+  }
   if (str.length <= length) { return str; }
   const subString = str.slice(0, length-1);
   return (useWordBoundary
