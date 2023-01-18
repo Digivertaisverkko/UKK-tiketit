53fd6a32a5a817a74b17697d213588de56d02189
Henri K <12832691+nkahe@users.noreply.github.com>
2023-01-18 13:23:53 +0200

    kirjautumistieto oikeaan ylänurkkaan, jos ollaan upotuksessa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './core/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +10,27 @@ import { AuthService } from './core/auth.service';
 export class AppComponent implements OnInit {
   public isPhonePortrait = false;
   public isInIframe: boolean = false;
+  // public isUserLoggedIn$: Observable<boolean>;
+  public loggingStatus: string = ''
 
   constructor(
       private authService: AuthService
-  ) {}
+  ) {
+    // this.isUserLoggedIn$ = this.authService.onIsUserLoggedIn();
+  }
+
   ngOnInit(): void {
     this.isInIframe = this.testIframe();
     window.sessionStorage.setItem('IN-IFRAME', this.isInIframe.toString());
+    console.log(' iframe upotuksen tila: ' + this.isInIframe.toString());
     this.authService.initialize();
+    this.authService.onIsUserLoggedIn().subscribe(response => {
+      if (response == true) {
+        this.loggingStatus = "Olet kirjautunut";
+      } else if (response == false) {
+        this.loggingStatus = "Et ole kirjautunut";
+      }
+    });
   }
 
   testIframe () {
