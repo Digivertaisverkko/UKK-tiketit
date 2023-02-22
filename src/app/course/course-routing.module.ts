6a12a15dddf69890367ea2d0448fb98405badbeb
henri <12832691+nkahe@users.noreply.github.com>
2023-02-22 16:23:37 +0200

    add: settings -kompontti ja routing

--- src/app/course/course-routing.module.ts
+++ src/app/course/course-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from '../user-management/auth.guard';
+import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
-
+  { path: 'course/:courseid/settings', component: SettingsComponent, canActivate: [authGuard] },
 ];
 
 // { path: '', component: AppComponent }
--- src/app/course/course.module.ts
+++ src/app/course/course.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CourseRoutingModule } from './course-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { SettingsComponent } from './settings/settings.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SettingsComponent
+  ],
   imports: [
     CommonModule,
     CourseRoutingModule,
