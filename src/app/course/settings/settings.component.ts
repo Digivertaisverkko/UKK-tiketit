a2ae466d93b3c207c9269f0264438892ebedd6d4
henri <12832691+nkahe@users.noreply.github.com>
2023-02-23 16:26:58 +0200

    fix: settings: yksikölliset namet input-kentille

--- src/app/course/settings/settings.component.ts
+++ src/app/course/settings/settings.component.ts
@@ -63,7 +63,7 @@ export class SettingsComponent implements OnInit {
     if (response.length > 0) {
       this.dataSource = new MatTableDataSource(response);
     }
-    console.log('luettiin luettiin tiedot: ' + JSON.stringify(response));
+    console.dir(this.dataSource);
   }
 
   private fetchTicketFieldInfo(courseID: string) {
