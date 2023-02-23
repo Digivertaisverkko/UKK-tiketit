e0c378c6f68ece2835d9a45fee0478fbd7643e47
Erkki Tervo <erkki.tervo@oulu.fi>
2023-02-23 14:04:23 +0200

    Editori: käyttäjä voi maalata nyt monirivisen koodinpätkän ja muuntaa sen lähdekoodiksi ja kaikki maalattu teksti muuttuu nyt oikealla tavalla yhden lähdekoodiblokin sisään

--- src/app/shared/editor/menu-src/menu-src.component.ts
+++ src/app/shared/editor/menu-src/menu-src.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { EditorState, Transaction } from 'prosemirror-state';
+import { EditorState, TextSelection, Transaction } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { setBlockType } from 'prosemirror-commands';
+import { findWrapping } from 'prosemirror-transform';
 
-import { Editor } from 'ngx-editor';
+
+import { Editor, schema } from 'ngx-editor';
 import { isNodeActive } from 'ngx-editor/helpers';
+import { NodeType } from 'prosemirror-model';
 
 @Component({
   selector: 'app-menu-src',
@@ -15,6 +18,7 @@ export class MenuSrcComponent implements OnInit {
   @Input() editor!: Editor;
   isActive = false;
   isDisabled = false;
+  nodeType!: NodeType;
 
   onClick(e: MouseEvent): void {
     e.preventDefault();
@@ -23,16 +27,39 @@ export class MenuSrcComponent implements OnInit {
   }
 
   execute(state: EditorState, dispatch?: (tr: Transaction) => void): boolean {
-    const { schema } = state;
+    const { doc, schema, selection, tr } = state;
+    const { empty, from, to } = selection;
 
     if (this.isActive) {
       return setBlockType(schema.nodes['paragraph'])(state, dispatch);
     }
 
+    if (!empty) {
+      // Napataan kaikki teksti valinnan sisällä ja asetetaan rivinvaihto blokkien väliin.
+      // Luodaan tästä tekstisisällöstä uusi node.
+      // TODO: korvataan rivinvaihtomerkit blokinvaihdolla, jos koodiblokista vaihdetaan tekstikappaleeksi.
+      const textContent = doc.textBetween(from, to, '\n');
+      const textContentNode = schema.text(textContent);
+
+      // Luodaan koodiblokkinode.
+      // Käytetään teksisisältönodea pohjana, jotta saadaan siirrettyä valinnan tekstisisältö koodiblokin sisään.
+      const { code_mirror } = state.schema.nodes;
+      const codeNode = code_mirror.create(null, textContentNode);
+
+      // Korvataan valittu alue uudella koodiblokkinodella.
+      tr.replaceSelectionWith(codeNode, false).scrollIntoView();
+
+      if (tr.docChanged) {
+        dispatch?.(tr);
+      }
+
+      return true;
+    }
+
     return setBlockType(schema.nodes['code_mirror'])(state, dispatch);
   }
 
-  update = (view: EditorView):void => {
+  update = (view: EditorView): void => {
     const { state } = view;
     const { schema } = state;
     this.isActive = isNodeActive(state, schema.nodes['code_mirror']);
