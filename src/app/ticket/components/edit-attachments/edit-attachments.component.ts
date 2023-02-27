0fcad84c5171c8f72dd8d7caed1ad168ca770e1c
Henri K <12832691+nkahe@users.noreply.github.com>
2023-02-27 13:35:45 +0200

    add: edit-attachments: tiedostolista sisältää mahd. virheilmoitukset.

--- src/app/ticket/components/edit-attachments/edit-attachments.component.ts
+++ src/app/ticket/components/edit-attachments/edit-attachments.component.ts
@@ -1,15 +1,20 @@
 import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
+interface FileInfo {
+  filename: string;
+  error?: string;
+}
+
 @Component({
   selector: 'app-edit-attachments',
   template: `
     <input type="file" class="file-input" multiple (change)="onFileChanged($event)" #fileUpload>
 
     <div class="file-list" *ngIf="fileList !== null">
-      <div *ngFor="let file of fileNameList; let index = index">
+      <div *ngFor="let file of fileInfoList; let index = index">
         <div class="list-item">
-          <span class="filename" matTooltip="{{file}}" [matTooltipShowDelay]="600">{{file}}</span>
+          <span class="filename" matTooltip="{{file.filename}}" [matTooltipShowDelay]="600">{{file.filename}}</span>
           <button mat-icon-button class="remove-file-button" (click)="removeSelectedFile(index)"><mat-icon>close</mat-icon></button>
         </div>
         <!-- <mat-error>Virheilmoitukset tähän.</mat-error> -->
@@ -18,12 +23,14 @@ import { Observable } from 'rxjs';
   styleUrls: ['./edit-attachments.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+
 export class EditAttachmentsComponent implements OnInit {
 
   @Output() fileListOutput = new EventEmitter<File[]>();
   @Input() uploadClicks: Observable<void> = new Observable();
   public fileList: File[] = [];
-  public fileNameList: string[] = [];
+  public fileInfoList: FileInfo[] = [];
+  // public fileNameList: string[] = [];
   public noAttachmentsMessage = $localize `:@@Ei liitetiedostoa:Ei liitetiedostoa` + '.';
 
   ngOnInit() {
@@ -33,16 +40,19 @@ export class EditAttachmentsComponent implements OnInit {
 
   public onFileChanged(event: any) {
     for (let file of event.target.files) {
-      if (this.fileNameList.includes(file.name)) continue;
+      console.log('file: ' + file);
+      // if (this.fileInfoList.filename.includes(file)) continue;
+      if (this.fileInfoList.some(fileinfo => fileinfo.filename === file.name)) continue
       this.fileList.push(file);
-      this.fileNameList.push(file.name);
+      this.fileInfoList.push({filename: file.name });
       this.fileListOutput.emit(this.fileList);
+      console.log('fileinfolist ' + JSON.stringify(this.fileInfoList));
     }
   }
 
   public removeSelectedFile(index: number) {
     this.fileList.splice(index, 1);
-    this.fileNameList.splice(index, 1);
+    this.fileInfoList.splice(index, 1);
     this.fileListOutput.emit(this.fileList);
   }
 
