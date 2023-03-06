cd0adadd061b47c98a299013d0ceed073d4aa54e
Henri K <12832691+nkahe@users.noreply.github.com>
2023-03-06 15:57:56 +0200

    asetukset-näkymästä tietojen lataamisen viive pois.
    backendin puolella pitäisi olla korjattu ettei tätä tarvitse.

--- src/app/course/settings/settings.component.ts
+++ src/app/course/settings/settings.component.ts
@@ -27,7 +27,7 @@ export class SettingsComponent implements OnInit {
   public isLoaded: boolean = false;
   public courseID: string = '';
   public courseName: string = '';
-  private delayFetching: string = window.history.state.delayFetching ?? false;
+  // private delayFetching: string = window.history.state.delayFetching ?? false;
 
   constructor(
     private router: Router,
@@ -51,11 +51,11 @@ export class SettingsComponent implements OnInit {
       this.courseID = courseID;
       this.showCourseName(this.courseID);
       // FIXME: Palvelin voi palauttaa tyhjän taulun, niin väliaikainen fiksi.
-      if (this.delayFetching == 'true') {
-        setTimeout(() => { this.fetchTicketFieldInfo(this.courseID) }, 200);
-      } else {
+      // if (this.delayFetching == 'true') {
+        // setTimeout(() => { this.fetchTicketFieldInfo(this.courseID) }, 200);
+      // } else {
       this.fetchTicketFieldInfo(courseID);
-      }
+      // }
       this.isLoaded = true;
     });
   }
