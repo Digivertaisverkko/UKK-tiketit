6693ecf901a322fba496b93dde5731abcd680266
henri <12832691+nkahe@users.noreply.github.com>
2023-03-08 14:07:19 +0200

    fix: route "course/:course-id" ohjaa listaan.
    lista: course/:course-id/list-tickets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { authGuard } from './user-management/auth.guard';
 import { PageNotFoundComponent } from './core/page-not-found/page-not-found.component';
+import { ListingComponent } from './ticket/listing/listing.component';
 
 // ? viimeisimmän kurssin muistaminen, jos on tallennettuna local storageen?
 // courseid:n nappaaminen routesta ei onnistunut, jos käytti loppuosasta vain wildcardia.
 const routes: Routes = [
   { path: '', redirectTo: '/course/1/list-tickets', pathMatch: 'full' },
+  { path: 'course/:courseid', pathMatch: 'full', redirectTo: '/course/:courseid/list-tickets' },
   { path: 'course/:courseid/:any',  component: PageNotFoundComponent },
   { path: 'course/:courseid/:any/:any',  component: PageNotFoundComponent },
   { path: 'course/:courseid/:any/:any/:any',  component: PageNotFoundComponent },
