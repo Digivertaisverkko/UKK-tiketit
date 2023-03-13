f119b02640e22d19cdc506b96f3e6be6c6eba3b9
Erkki Tervo <erkki.tervo@oulu.fi>
2023-03-13 12:29:43 +0200

    profile: profiilisivun komponentin luominen sekä alustava reititys profile/:profileid

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
+import { MatDividerModule }  from '@angular/material/divider';
+
 import { SharedModule } from '../shared/shared.module';
 
 import { LoginComponent } from './login/login.component';
-import { UserManagementRoutingModule } from './user-management-routing.module';
-
-import { MatDividerModule }  from '@angular/material/divider';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
+import { UserManagementRoutingModule } from './user-management-routing.module';
 
 @NgModule({
   declarations: [
     LoginComponent,
+    ProfileComponent,
     RegisterComponent
   ],
   imports: [
