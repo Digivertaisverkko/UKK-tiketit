780f37280158b66b3a0deedcc7508f7638e7b43c
Henri K <12832691+nkahe@users.noreply.github.com>
2023-03-20 16:54:32 +0200

    edit-attachments: koodin siivoilua

--- src/app/ticket/components/edit-attachments/edit-attachments.component.ts
+++ src/app/ticket/components/edit-attachments/edit-attachments.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter, OnInit,
+        ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { forkJoin, Observable, pipe, map, tap, catchError, of, throwError } from 'rxjs';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { TicketService } from '../../ticket.service';
@@ -21,9 +22,11 @@ interface FileInfo {
 @Component({
   selector: 'app-edit-attachments',
   template: `
-    <input type="file" class="file-input" #fileInput multiple (change)="onFileChanged($event)" #fileUpload>
+    <input class="file-input" (change)="onFileChanged($event)" #fileInput #fileUpload
+        type="file" multiple>
 
-    <!-- <input type="file" class="file-input" id="file-input-{{url}}" multiple name="file-input-{{url}}"
+    <!-- <input type="file" class="file-input" id="file-input-{{url}}" multiple
+       name="file-input-{{url}}"
       (change)="onFileChanged($event)" #fileUpload> -->
 
     <p class="status-message-p" *ngIf="userMessage">{{userMessage}}</p>
@@ -35,8 +38,9 @@ interface FileInfo {
           <span class="filename" matTooltip="{{file.filename}}" [matTooltipShowDelay]="600">
             {{file.filename}}</span>
             <span style="font-weight: 300">&nbsp;({{file.filesize | filesize }})</span>
-          <div class="file-error-message" matError *ngIf="file.error" matTooltip="{{file?.errorToolTip}}"
-            [matTooltipShowDelay]="600"><mat-icon>warning</mat-icon>{{file.error}}
+          <div class="file-error-message" matError *ngIf="file.error"
+              matTooltip="{{file?.errorToolTip}}" [matTooltipShowDelay]="600">
+              <mat-icon>warning</mat-icon>{{file.error}}
           </div>
 
           <button mat-icon-button [disabled]="isEditingDisabled" class="remove-file-button"
@@ -44,11 +48,13 @@ interface FileInfo {
             <mat-icon>close</mat-icon>
           </button>
         </div>
-        <mat-icon class="ok-icon" *ngIf="file.progress === 100 && !file.uploadError">done</mat-icon>
+        <mat-icon class="ok-icon" *ngIf="file.progress === 100 && !file.uploadError">
+          done
+        </mat-icon>
         <mat-icon class="error-icon" *ngIf="file.uploadError">error</mat-icon>
         <span *ngIf="file.uploadError">{{file.uploadError}}</span>
-        <mat-progress-bar [value]="file.progress" *ngIf="file.progress && !file.uploadError"
-          mode="determinate" value=100 >
+        <mat-progress-bar [value]="file.progress" mode="determinate" value=100
+            *ngIf="file.progress && !file.uploadError">
         </mat-progress-bar>
       </div>
     </div>`,
@@ -105,55 +111,41 @@ export class EditAttachmentsComponent implements OnInit {
       };
       if (file.size > this.MAX_FILE_SIZE_MB * MEGABYTE) {
         fileinfo.error = $localize `:@@Liian iso:Liian iso`;
-        fileinfo.errorToolTip = $localize `:@@Tiedoston koko ylittää:Tiedoston koko ylittää
+        fileinfo.errorToolTip = $localize `:@@Tiedoston koko ylittää:
+            Tiedoston koko ylittää
           ${this.MAX_FILE_SIZE_MB} megatavun rajoituksen` + '.';
         this.attachmentsMessages.emit('faulty');
       }
-      // this.fileList.push(file);
-      // console.log('saatiin fileinfo (alla):');
-      // console.dir(fileinfo);
-      // console.log('koko lista:');
-      // console.dir(this.fileInfoList);
       this.fileInfoList.push(fileinfo);
       this.fileListOutput.emit(this.fileInfoList);
-      // console.log('fileinfolist ' + JSON.stringify(this.fileInfoList));
-      // console.log('filelist:');
-      // console.dir(this.fileList);
     }
   }
 
   private makeRequestArray(ticketID: string, commentID: string): any {
     return this.fileInfoList.map((fileinfo, index) => {
-        return this.ticketService.newUploadFile(ticketID, commentID, fileinfo.file).pipe(
-          // return this.ticketService.uploadError(ticketID, commentID, fileinfo.file).pipe(
-          tap(progress => {
-            console.log('saatiin event (alla) tiedostolle ('+ fileinfo.filename + '): ' + progress);
-            this.fileInfoList[index].progress = progress;
-            // console.dir(event)
-            // if (event.type === HttpEventType.UploadProgress) {
-            //   this.fileInfoList[index].progress = Math.round(100 * event.loaded / event.total);
-            // }
-          }),
-          catchError((error: any) => {
-            console.log('makeRequestChain: catchError: error napattu');
-            this.fileInfoList[index].uploadError = $localize `:@@Liitteen lähettäminen epäonnistui:Liitteen lähettäminen epäonnistui.`;
-            // Koko upload loppuu kaikkien tiedostojen kohdalla jos heitetään virhe.
-            return of('error');
-            // return throwError( () => new Error(error) );
-          })
-        )
+      return this.ticketService.newUploadFile(ticketID, commentID, fileinfo.file)
+      .pipe(
+        // return this.ticketService.uploadError(ticketID, commentID, fileinfo.file).pipe(
+        tap(progress => {
+          console.log('saatiin event (alla) tiedostolle ('+ fileinfo.filename +
+              '): ' + progress);
+          this.fileInfoList[index].progress = progress;
+          // console.dir(event)
+          // if (event.type === HttpEventType.UploadProgress) {
+          //   this.fileInfoList[index].progress = Math.round(100 * event.loaded / event.total);
+          // }
+        }),
+        catchError((error: any) => {
+          console.log('makeRequestChain: catchError: error napattu');
+          this.fileInfoList[index].uploadError = $localize `:@@Liitteen
+              lähettäminen epäonnistui:Liitteen lähettäminen epäonnistui.`;
+          // Koko upload loppuu kaikkien tiedostojen kohdalla jos heitetään virhe.
+          return of('error');
+          // return throwError( () => new Error(error) );
+        })
+      )
     });
   }
-
-  // public sendFiles(ticketID: string, commentID: string): Observable<any> {
-  //   forkJoin(uploadObservables)
-  //     .pipe(
-  //       map(results => {
-  //         console.log('forkjoin: saatiin tulokset:');
-  //         return results
-  //       })
-  //     );
-  // }
 
   public async sendFilesPromise(ticketID: string, commentID: string): Promise<any> {
     this.isEditingDisabled = true;
@@ -175,72 +167,6 @@ export class EditAttachmentsComponent implements OnInit {
       });
     })
   }
-
-  // Ei käytössä atm.
-  public sendFiles(ticketID: string, commentID: string) {
-    this.isEditingDisabled = true;
-    let requestChain = this.makeRequestArray(ticketID, commentID)
-  //   .pipe(result:any) => {
-  //     if (this.fileInfoList.some(fileInfo => fileInfo.uploadError)) {
-  //       return throwError( () => new Error(result) );
-  //     }
-  //     return of(result)
-  //   }
-  // );
-    return forkJoin(requestChain)
-  }
-
- /*  public sendFiles(ticketID: string, commentID: string): Promise<boolean> {
-    this.isEditingDisabled = true;
-    this.userMessage = $localize `:@@ähetetään liitetiedostoja:Lähetetään liitetiedostoja, ole hyvä ja odota hetki...`;
-    // Koko transaktio.
-    return new Promise((resolve, reject) => {
-      console.log('edit-attachments: ticketID: ' + ticketID + ' commentID: ' + commentID);
-      for (let [index, file] of this.fileInfoList.entries()) {
-          this.ticketService.uploadFile(ticketID, commentID, file).subscribe({
-          next: (progress) => {
-            // Progress barin päivitys.
-            if (progress > 0 && this.fileInfoList[index]) this.fileInfoList[index].progress = progress;
-            if (progress === 100) this.fileInfoList[index].done = true;
-          },
-          error: (error) => {
-            this.fileInfoList[index].done = true;
-            console.log('saatiin virhe: ' + error);
-            this.attachmentsMessages.emit('errors');
-            console.error('tämä epäonnistui: ' + this.fileInfoList[index].filename);
-            this.fileInfoList[index].uploadError = "Tiedoston lähettäminen ei onnistunut.";
-          },
-          complete: () => {
-            console.log('Tiedosto valmis!');
-            // if (progress?.success) {
-              this.fileInfoList[index].done = true;
-              // if (progress.success == true) {
-              //   console.log('Tämä onnistui: '+ this.fileInfoList[index].filename);
-              // } else {
-              //   console.log('Tämä epäonnistui: '+ this.fileInfoList[index].filename);
-              // }
-              // Jos kaikki valmista.
-              for (let file of this.fileInfoList) {
-                console.log('Tiedosto: ' + file.filename + '  valmis? ' + file.done ?? '');
-              }
-              if (this.fileInfoList.every(fileinfo => {
-                fileinfo?.done === true || fileinfo.progress == 100
-              })) {
-                console.log('kaikki lähetetty');
-                this.userMessage = "Valmista!";
-                if (this.fileInfoList.some(fileinfo => fileinfo.uploadError)) {
-                  this.userMessage += ' Kaikkien tiedostojen lähettäminen ei onnistunut.';
-                  this.attachmentsMessages.emit('errors');
-                } else {
-                  this.attachmentsMessages.emit('done');
-                }
-              }
-            // }
-          }
-        }); // Subscribe
-      }  // For
-    }) // Promise
-  } */
 
   public removeSelectedFile(index: number) {
     // this.fileList.splice(index, 1);
