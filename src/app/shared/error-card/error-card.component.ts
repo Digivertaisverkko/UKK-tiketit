256900e26209b1e626c643b9d22e0d7d58e46e2d
henri <12832691+nkahe@users.noreply.github.com>
2023-03-22 10:27:23 +0200

    virheilmoitusten ulkonäkö

--- src/app/shared/error-card/error-card.component.ts
+++ src/app/shared/error-card/error-card.component.ts
@@ -1,16 +1,25 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
 @Component({
   selector: 'app-error-card',
-  templateUrl: './error-card.component.html',
+  template: `
+
+    <div class="border-area">
+      <div class="headline-wrapper">
+      <mat-icon fontIcon="warning_amber"></mat-icon>
+        <h4 class="h4">{{ title }}</h4>
+      </div>
+      <p>{{ message }}</p>
+    </div>
+
+  `,
   styleUrls: ['./error-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ErrorCardComponent {
 
-  @Input() title: string = 'Virhe';
   @Input() message: string = 'Toiminto epäonnistui.';
   @Input() styles: object = {};
+  @Input() title: string = 'Virhe';
 
   constructor() { }
 
