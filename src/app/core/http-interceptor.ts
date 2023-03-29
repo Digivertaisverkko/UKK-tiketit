e833efdc17be56410a4a3c2626d772d13d939778
henri <12832691+nkahe@users.noreply.github.com>
2023-03-29 14:04:04 +0300

    remove: http-interceptor: session id lähetys

--- src/app/core/http-interceptor.ts
+++ src/app/core/http-interceptor.ts
@@ -8,16 +8,6 @@ import { truncate } from '../utils/truncate';
 // Kaikkien HTTP-pyyntöjen yhteydessä tehtävät toimet.
 export class CustomHttpInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const SESSION_ID = window.localStorage.getItem('SESSION_ID');
-    // console.log('session id on: ' + sessionID);
-    // var sessionID = '123456789';
-    if (SESSION_ID === undefined || SESSION_ID === null) {
-      console.warn('getHttpOptions: ei session ID:ä.');
-    } else {
-      request = request.clone({
-        headers: request.headers.set('session-id', SESSION_ID),
-      });
-    }
     return next.handle(request).pipe(
       tap(event => {
         if (event instanceof HttpResponse) {
