242282efcf4906f0970eaff3c54c6a531e0f072a
Henri K <12832691+nkahe@users.noreply.github.com>
2023-04-03 15:25:57 +0300

    isValidHttpUrl funktio listing -> utils.ts

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -21,3 +21,14 @@ export function getCourseIDfromURL(): string | null {
   }
   return courseID
 }
+
+  // Onko string muodoltaan HTTP URL.
+  export function isValidHttpUrl(testString: string): boolean {
+    let url: URL;
+    try {
+      url = new URL(testString);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
