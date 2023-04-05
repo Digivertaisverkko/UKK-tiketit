1587b22aaf8ff32f3102e3a891c17de5c06e7aad
henri <12832691+nkahe@users.noreply.github.com>
2023-04-05 13:28:15 +0300

    ticketRoutingModule: koodin muotoilu

--- src/app/ticket/ticket-routing.module.ts
+++ src/app/ticket/ticket-routing.module.ts
@@ -9,13 +9,31 @@ import { FaqViewComponent } from './faq-view/faq-view.component';
 import { ListingComponent } from './listing/listing.component';
 
 const routes: Routes = [
-  { path: 'course/:courseid/ticket-view/:id', component: TicketViewComponent, canActivate: [authGuard] },
-  { path: 'course/:courseid/submit', component: SubmitTicketComponent, canActivate: [authGuard] },
-  { path: 'course/:courseid/submit/:id', component: SubmitTicketComponent, canActivate: [authGuard] },
-  { path: 'course/:courseid/submit-faq', component: SubmitFaqComponent, canActivate: [authGuard] },
-  { path: 'course/:courseid/submit-faq/:id', component: SubmitFaqComponent, canActivate: [authGuard] },
-  { path: 'course/:courseid/faq-view/:id', component: FaqViewComponent },
-  { path: 'course/:courseid/list-tickets', component: ListingComponent },
+  { path: 'course/:courseid/ticket-view/:id',
+      component: TicketViewComponent,
+      canActivate: [authGuard] },
+
+  { path: 'course/:courseid/submit',
+      component: SubmitTicketComponent,
+      canActivate: [authGuard] },
+
+  { path: 'course/:courseid/submit/:id',
+      component: SubmitTicketComponent,
+      canActivate: [authGuard] },
+
+  { path: 'course/:courseid/submit-faq',
+      component: SubmitFaqComponent,
+      canActivate: [authGuard] },
+
+  { path: 'course/:courseid/submit-faq/:id',
+      component: SubmitFaqComponent,
+      canActivate: [authGuard] },
+
+  { path: 'course/:courseid/faq-view/:id',
+      component: FaqViewComponent },
+
+  { path: 'course/:courseid/list-tickets',
+      component: ListingComponent },
 ];
 //   { path: 'list-tickets', component: ListingComponent }
 
