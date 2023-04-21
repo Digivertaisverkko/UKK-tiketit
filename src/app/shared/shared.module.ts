00f6ffe016971ce81a96b7694fcc0cb555786302
Erkki Tervo <erkki.tervo@oulu.fi>
2023-04-21 14:00:39 +0300

    Lisätään ReactiveForms moduuli

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -69,6 +69,7 @@ export function ngxEditorConfigFactory(): NgxEditorConfig {
     MenuSrcComponent,
     ToBeginningButtonComponent,
     SafeHtmlPipe,
+    ReactiveFormsModule,
     SearchBarComponent,
   ],
   providers: [
