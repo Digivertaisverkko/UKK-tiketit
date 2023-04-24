5a8d2f1019923633a112763edfb34e930a54d5f5
henri <12832691+nkahe@users.noreply.github.com>
2023-04-24 09:02:00 +0300

    suodatuskenttä: template komponentin kanssa samaan tiedostoon

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,18 +1,42 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import {  Component, ChangeDetectionStrategy, ElementRef, EventEmitter, Output,
+          ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss']
+  template: `
+    <div class="search-bar">
+
+      <input  class="input"
+              #input
+              type="text"
+              (keyup)="searchResult.emit($event)"
+              placeholder="&nbsp;"
+              >
+
+      <span class="label">
+        <ng-content></ng-content>
+      </span>
+
+      <span class="highlight"></span>
+
+      <button aria-disabled="true"
+              class="search-btn"
+              (click)="searchInput.nativeElement.focus()"
+              mat-icon-button
+              tabindex="-1"
+              >
+        <mat-icon>search</mat-icon>
+      </button>
+
+    </div>
+  `,
+  styleUrls: ['./search-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 export class SearchBarComponent {
 
   @ViewChild('input') searchInput!: ElementRef<HTMLInputElement>;
   @Output() searchResult = new EventEmitter<Event>();
-
-  public focus() {
-    this.searchInput.nativeElement.focus();
-  }
 
 }
