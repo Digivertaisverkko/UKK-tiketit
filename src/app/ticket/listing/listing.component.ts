48de4d8967220d78b44eaf51416f250c0f4c5b7a
Henri K <12832691+nkahe@users.noreply.github.com>
2023-04-25 11:06:39 +0300

    fix: kirjautumis-virheilmoituksen päivitys.
    Jos ensin tulee tieto kirjautumattomuudesta ja annetaan virhe, korjaa jos saadaan uusi tieto.

--- src/app/ticket/listing/listing.component.ts
+++ src/app/ticket/listing/listing.component.ts
@@ -293,6 +293,7 @@ export class ListingComponent implements OnInit, AfterViewInit, OnDestroy {
         this.isLoaded = true;
         this.setError('notLoggedIn');
       } else if (response === true) {
+        this.error === null;
         this.checkIfParticipant();
       }
     });
