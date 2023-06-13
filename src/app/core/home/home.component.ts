d67c7ea102fd000754f22c16cd94b42975d570c9
henri <12832691+nkahe@users.noreply.github.com>
2023-06-13 12:41:20 +0300

    add: /home: headerin logo klikattavaksi.

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { StoreService } from '@core/services/store.service';
+
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent {
 
+  @Input() courseid: string | null = null;
   public lang: string | null;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private store: StoreService
+  ) {
+    // Kun esim. headerin logoa klikataan.
+    this.store.trackMessages().pipe(
+      takeUntilDestroyed()).subscribe(response => {
+        if (response === 'go begin') {
+          const route = `course/${this.courseid}/list-tickets`;
+          this.router.navigateByUrl(route);
+        }
+      });
     this.lang = localStorage.getItem('language')?.substring(0,2) ?? 'fi';
   }
+
 
 }
