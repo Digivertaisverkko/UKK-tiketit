a5257e005f687c121fa593ccd26694893d9f1218
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-15 16:45:57 +0300

    register: test 'should create'

--- src/app/user/register/register.component.spec.ts
+++ src/app/user/register/register.component.spec.ts
@@ -1,20 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed
+    } from '@angular/core/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MockComponent } from 'ng-mocks';
 
-import { RegisterComponent } from './register.component';
+import { AuthService } from '@core/services/auth.service';
+import { StoreService } from '@core/services/store.service';
+import { CourseService } from '@course/course.service';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
+import { RegisterComponent } from '@user/register/register.component';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeCourseService: jasmine.SpyObj<CourseService>;
+  let fakeStoreService: jasmine.SpyObj<StoreService>;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      createAccount:undefined,
+      getLoginInfo: undefined,
+      login: undefined,
+      logout: undefined
+    });
+
+    fakeCourseService = jasmine.createSpyObj('CourseService', {
+      getCourseName: undefined,
+      getInvitedInfo: undefined
+    });
+
+    fakeStoreService = jasmine.createSpyObj('StoreService', {
+      getBaseTitle: undefined,
+      onIsUserLoggedIn: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [
+        MockComponent(HeadlineComponent),
+        RegisterComponent
+      ],
+      imports: [
+        MatFormFieldModule
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: CourseService, useValue: fakeCourseService },
+        { provide: StoreService, useValue: fakeStoreService }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
