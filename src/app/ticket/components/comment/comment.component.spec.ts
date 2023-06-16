29472d6e3437bc9ad689e5a0751f8d43360bf4cf
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 23:36:46 +0300

    comment: testi 'should create' onnistuu

--- src/app/ticket/components/comment/comment.component.spec.ts
+++ src/app/ticket/components/comment/comment.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MockComponent } from 'ng-mocks';
 
 import { CommentComponent } from './comment.component';
+import { SenderInfoComponent } from '@shared/components/sender-info/sender-info.component';
+import { TicketService } from '@ticket/ticket.service';
+import { EditAttachmentsComponent } from '@ticket/components/edit-attachments/edit-attachments.component';
+import { MessageComponent } from '@ticket/components/message/message.component';
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<CommentComponent>;
 
   beforeEach(async () => {
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      editComment: undefined,
+      removeComment: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ CommentComponent ]
+      declarations: [
+        CommentComponent,
+        MockComponent(EditAttachmentsComponent),
+        MockComponent(MessageComponent),
+        MockComponent(SenderInfoComponent)
+      ],
+      imports: [
+        MatCardModule,
+        MatIconModule
+      ],
+      providers: [
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
