e2d0419a6ff7f2c2e3977207ec485ddc8fe5a401
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:16:28 +0300

    ticket-list: testi 'should create' onnistuu

--- src/app/ticket/listing/ticket-list/ticket-list.component.spec.ts
+++ src/app/ticket/listing/ticket-list/ticket-list.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
 
 import { TicketListComponent } from './ticket-list.component';
+import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
+import { TicketService } from '@ticket/ticket.service';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('TicketListComponent', () => {
   let component: TicketListComponent;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<TicketListComponent>;
 
   beforeEach(async () => {
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      getTicketList: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ TicketListComponent ]
+      declarations: [
+        MockComponent(SearchBarComponent),
+        TicketListComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatSortModule,
+        MatTableModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
