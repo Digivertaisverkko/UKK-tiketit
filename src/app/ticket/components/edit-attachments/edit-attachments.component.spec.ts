2954c98cb8c93592d099973a1b5ecb584460d20b
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 23:41:11 +0300

    edit-attachments: testi 'should create' onnistuu

--- src/app/ticket/components/edit-attachments/edit-attachments.component.spec.ts
+++ src/app/ticket/components/edit-attachments/edit-attachments.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditAttachmentsComponent } from './edit-attachments.component';
+import { TicketService } from '@ticket/ticket.service';
 
 describe('EditAttachmentsComponent', () => {
   let component: EditAttachmentsComponent;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<EditAttachmentsComponent>;
 
   beforeEach(async () => {
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      uploadFile: undefined,
+      removeFile: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ EditAttachmentsComponent ]
+      declarations: [ EditAttachmentsComponent ],
+      providers: [
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
