8dba21dcc3225b822395c0463d375194d9d58904
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 15:55:54 +0300

    no-data-consent: testi 'should create' onnistuu

--- src/app/core/no-data-consent/no-data-consent.component.spec.ts
+++ src/app/core/no-data-consent/no-data-consent.component.spec.ts
@@ -1,20 +1,46 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed
+    } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MockComponent } from 'ng-mocks';
 
 import { NoDataConsentComponent } from './no-data-consent.component';
+import { AuthService } from '@core/services/auth.service';
+import { StoreService } from '@core/services/store.service';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
 
 describe('NoDataConsentComponent', () => {
   let component: NoDataConsentComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeStoreService: jasmine.SpyObj<StoreService>;
   let fixture: ComponentFixture<NoDataConsentComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      removeDenyConsent: undefined
+    });
+
+    fakeStoreService = jasmine.createSpyObj('StoreService', {
+      getBaseTitle: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ NoDataConsentComponent ]
+      declarations: [
+        MockComponent(HeadlineComponent),
+        NoDataConsentComponent
+      ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: StoreService, useValue: fakeStoreService }
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(NoDataConsentComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
