519a3c55a4e1a66390fb55db8199f1ff9c0245b1
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 13:37:43 +0300

    data-consent: testi 'should create' onnistuu

--- src/app/core/data-consent/data-consent.component.spec.ts
+++ src/app/core/data-consent/data-consent.component.spec.ts
@@ -1,14 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, ComponentFixtureAutoDetect, TestBed
+    } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MockComponent } from 'ng-mocks';
 
 import { DataConsentComponent } from './data-consent.component';
+import { AuthService } from '@core/services/auth.service';
+import { StoreService } from '@core/services/store.service';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
 
 describe('DataConsentComponent', () => {
   let component: DataConsentComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeStoreService: jasmine.SpyObj<StoreService>;
   let fixture: ComponentFixture<DataConsentComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      sendDataConsent: undefined
+    });
+
+    fakeStoreService = jasmine.createSpyObj('StoreService', {
+      getBaseTitle: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ DataConsentComponent ]
+      declarations: [
+        DataConsentComponent,
+        MockComponent(HeadlineComponent)
+      ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true },
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: StoreService, useValue: fakeStoreService }
+      ]
     })
     .compileComponents();
 
