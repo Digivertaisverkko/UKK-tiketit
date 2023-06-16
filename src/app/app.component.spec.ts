0dba4e8381f7ae5210047cbdfa09287312e8e8f1
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 02:03:54 +0300

    app-component: testi 'should create' onnistuu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,40 @@
 import { TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
+
 import { AppComponent } from './app.component';
+import { FooterComponent } from '@core/footer/footer.component';
+import { AuthService } from '@core/services/auth.service';
+import { UsermenuComponent } from '@core/usermenu/usermenu.component';
 
 describe('AppComponent', () => {
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('CourseService', {
+      initialize: undefined
+    });
+
     await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        MockComponent(FooterComponent),
+        MockComponent(UsermenuComponent)
+      ],
       imports: [
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatTooltipModule,
         RouterTestingModule
       ],
-      declarations: [
-        AppComponent
-      ],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService }
+      ]
     }).compileComponents();
   });
 
@@ -20,16 +44,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-//  it(`should have as title 'tikettisysteemi'`, () => {
-//    const fixture = TestBed.createComponent(AppComponent);
-//    const app = fixture.componentInstance;
-//    expect(app.title).toEqual('tikettisysteemi');
-//  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tikettisysteemi app is running!');
-  });
 });
