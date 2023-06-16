edc2c4a47e73ba70edee4ae16ecdd09802840a75
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 15:48:36 +0300

    header-component: testi 'should create' onnistuu

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterTestingModule } from "@angular/router/testing";
+import { MockComponent } from 'ng-mocks';
+import { of } from 'rxjs';
 
 import { HeaderComponent } from './header.component';
+import { StoreService } from '@core/services/store.service';
+import { UsermenuComponent } from '@core/usermenu/usermenu.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
+  let fakeStoreService: jasmine.SpyObj<StoreService>;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
+    fakeStoreService = jasmine.createSpyObj('StoreService', {
+      sendMessage: undefined,
+      trackUserInfo: of('')
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [
+        HeaderComponent,
+        MockComponent(UsermenuComponent)
+      ],
+      imports: [
+        MatIconModule,
+        MatMenuModule,
+        MatToolbarModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: StoreService, useValue: fakeStoreService }
+      ]
     })
     .compileComponents();
 
