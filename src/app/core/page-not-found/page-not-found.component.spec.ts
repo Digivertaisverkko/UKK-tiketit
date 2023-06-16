e994fe921e699dde644a9971eefd8fca57a8c14e
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 16:04:51 +0300

    page-not-found: testi 'should create' onnistuu

--- src/app/core/page-not-found/page-not-found.component.spec.ts
+++ src/app/core/page-not-found/page-not-found.component.spec.ts
@@ -1,14 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthService } from '@core/services/auth.service';
+import { StoreService } from '@core/services/store.service';
+import { CourseService } from '@course/course.service';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeCourseService: jasmine.SpyObj<CourseService>;
+  let fakeStoreService: jasmine.SpyObj<StoreService>;
   let fixture: ComponentFixture<PageNotFoundComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      createAccount: undefined,
+      getLoginInfo: undefined,
+      login: undefined,
+      logout: undefined
+    });
+
+    fakeCourseService = jasmine.createSpyObj('CourseService', {
+      getCourseName: undefined,
+      getInvitedInfo: undefined
+    });
+
+    fakeStoreService = jasmine.createSpyObj('StoreService', {
+      getBaseTitle: undefined,
+      getIsLoggedIn: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ PageNotFoundComponent ]
+      declarations: [
+        MockComponent(HeadlineComponent),
+        PageNotFoundComponent
+      ],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: CourseService, useValue: fakeCourseService },
+        { provide: StoreService, useValue: fakeStoreService }
+      ]
     })
     .compileComponents();
 
