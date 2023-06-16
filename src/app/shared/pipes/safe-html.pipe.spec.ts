5e4f35f237bec41f9a78c7a173d717c6040851ca
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:57:54 +0300

    safe-html pipe: testi 'should create' onnistuu

--- src/app/shared/pipes/safe-html.pipe.spec.ts
+++ src/app/shared/pipes/safe-html.pipe.spec.ts
@@ -1,8 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+
 import { SafeHtmlPipe } from './safe-html.pipe';
 
-//describe('SafeHtmlPipe', () => {
-//  it('create an instance', () => {
-//    const pipe = new SafeHtmlPipe();
-//    expect(pipe).toBeTruthy();
-//  });
-//});
+describe('SafeHtmlPipe', () => {
+  let sanitizer: DomSanitizer;
+  let pipe: SafeHtmlPipe;
+
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+          imports: [BrowserModule]
+      });
+
+      sanitizer = TestBed.inject(DomSanitizer);
+      pipe = new SafeHtmlPipe(sanitizer);
+  });
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+});
