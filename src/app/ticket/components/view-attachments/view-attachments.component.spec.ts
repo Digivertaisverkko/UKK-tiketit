2fb4da125512b3198e529a787a25907b4e1eae1b
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:05:05 +0300

    view-attachments: testi 'should create' onnistuu

--- src/app/ticket/components/view-attachments/view-attachments.component.spec.ts
+++ src/app/ticket/components/view-attachments/view-attachments.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewAttachmentsComponent } from './view-attachments.component';
+import { TicketService } from '@ticket/ticket.service';
 
 describe('AttachmentListComponent', () => {
   let component: ViewAttachmentsComponent;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<ViewAttachmentsComponent>;
 
   beforeEach(async () => {
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      getFile: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ ViewAttachmentsComponent ]
+      declarations: [ ViewAttachmentsComponent ],
+      providers: [
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
