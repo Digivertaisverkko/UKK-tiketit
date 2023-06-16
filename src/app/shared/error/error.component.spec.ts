297728f3235e701ac712f58e44de0d4203dfaa25
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:48:51 +0300

    error: testi 'should create' onnistuu

--- src/app/shared/error/error.component.spec.ts
+++ src/app/shared/error/error.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorComponent } from './error.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('ErrorCardComponent', () => {
   let component: ErrorComponent;
@@ -8,7 +9,10 @@ describe('ErrorCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorComponent ]
+      declarations: [ ErrorComponent ],
+      imports: [
+        MatIconModule
+      ]
     })
     .compileComponents();
 
