9fd6f18ca9fd781e139504a7aced5d2c7847bb8f
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 18:27:43 +0300

    edit-field: testi 'should create' onnistuu

--- src/app/course/edit-field/edit-field.component.spec.ts
+++ src/app/course/edit-field/edit-field.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
 
 import { EditFieldComponent } from './edit-field.component';
+import { CourseService } from '@course/course.service';
+import { BeginningButtonComponent } from '@shared/components/beginning-button/beginning-button.component';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
 
 describe('EditFieldComponent', () => {
   let component: EditFieldComponent;
+  let fakeCourseService: jasmine.SpyObj<CourseService>;
   let fixture: ComponentFixture<EditFieldComponent>;
 
   beforeEach(async () => {
+    fakeCourseService = jasmine.createSpyObj('CourseService', {
+      getTicketFieldInfo: undefined,
+      setTicketField: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ EditFieldComponent ]
+      declarations: [
+        EditFieldComponent,
+        MockComponent(BeginningButtonComponent),
+        MockComponent(HeadlineComponent),
+      ],
+      providers: [
+        { provide: CourseService, useValue: fakeCourseService }
+      ]
     })
     .compileComponents();
 
