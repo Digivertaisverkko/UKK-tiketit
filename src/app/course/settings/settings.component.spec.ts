c9070527daf01072350758eef8786f3a65d9b77f
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 18:46:11 +0300

    settings: testi 'should create' onnistuu

--- src/app/course/settings/settings.component.spec.ts
+++ src/app/course/settings/settings.component.spec.ts
@@ -1,14 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockComponent } from 'ng-mocks';
 
 import { SettingsComponent } from './settings.component';
+import { CourseService } from '@course/course.service';
+import { BeginningButtonComponent } from '@shared/components/beginning-button/beginning-button.component';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
 
 describe('SettingsComponent', () => {
   let component: SettingsComponent;
+  let fakeCourseService: jasmine.SpyObj<CourseService>;
   let fixture: ComponentFixture<SettingsComponent>;
 
   beforeEach(async () => {
+    fakeCourseService = jasmine.createSpyObj('CourseService', {
+      getCourseName: undefined,
+      getInvitedInfo: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [
+        MockComponent(BeginningButtonComponent),
+        MockComponent(HeadlineComponent),
+        SettingsComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatRadioModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: CourseService, useValue: fakeCourseService }
+      ]
     })
     .compileComponents();
 
