e1649369b04a962e9f304e854e042b0c8d835b2b
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:09:11 +0300

    faq-view: testi 'should create' onnistuu

--- src/app/ticket/faq-view/faq-view.component.spec.ts
+++ src/app/ticket/faq-view/faq-view.component.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
 
 import { FaqViewComponent } from './faq-view.component';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
+import { TicketService } from '@ticket/ticket.service';
 
 describe('FaqViewComponent', () => {
   let component: FaqViewComponent;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<FaqViewComponent>;
 
   beforeEach(async () => {
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      archiveFaq: undefined,
+      getTicket: Promise.resolve('')
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ FaqViewComponent ]
+      declarations: [
+        FaqViewComponent,
+        MockComponent(HeadlineComponent)
+      ],
+      providers: [
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
