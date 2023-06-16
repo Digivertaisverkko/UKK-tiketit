d044d795753ceb2bb4f00b123ca1485162a87f2b
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 00:33:51 +0300

    listing: testi 'should create' onnistuu

--- src/app/ticket/listing/listing.component.spec.ts
+++ src/app/ticket/listing/listing.component.spec.ts
@@ -1,14 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockComponent } from 'ng-mocks';
 
 import { ListingComponent } from './listing.component';
+import { AuthService } from '@core/services/auth.service';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
+import { TicketService } from '@ticket/ticket.service';
 
 describe('ListingComponent', () => {
   let component: ListingComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<ListingComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      getDenyDataConsent: undefined,
+      navigateToLogin: undefined
+    });
+
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      getFAQList: undefined
+    });
+
+    // Rivi 133: checkSuccessMessage()
+    window.history.pushState({ message: ''}, '', '');
+
+
     await TestBed.configureTestingModule({
-      declarations: [ ListingComponent ]
+      declarations: [
+        ListingComponent,
+        MockComponent(HeadlineComponent)
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MatDialogModule,
+        MatSortModule,
+        MatTableModule,
+        RouterTestingModule
+      ],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
