9af54f1039a05d7a158e94feb6a5514a1e299fe7
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 21:46:01 +0300

    submit-faq: testi 'should create' onnistuu

--- src/app/ticket/submit-faq/submit-faq.component.spec.ts
+++ src/app/ticket/submit-faq/submit-faq.component.spec.ts
@@ -1,14 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockComponent } from 'ng-mocks';
 
 import { SubmitFaqComponent } from './submit-faq.component';
+import { CourseService } from '@course/course.service';
+import { BeginningButtonComponent } from '@shared/components/beginning-button/beginning-button.component';
+import { HeadlineComponent } from '@shared/components/headline/headline.component';
+import { EditorComponent } from '@shared/editor/editor.component';
+import { TicketService } from '@ticket/ticket.service';
+import { EditAttachmentsComponent } from '@ticket/components/edit-attachments/edit-attachments.component';
 
 describe('SubmitFaqComponent', () => {
   let component: SubmitFaqComponent;
+  let fakeCourseService: jasmine.SpyObj<CourseService>;
+  let fakeTicketService: jasmine.SpyObj<TicketService>;
   let fixture: ComponentFixture<SubmitFaqComponent>;
 
   beforeEach(async () => {
+    fakeCourseService = jasmine.createSpyObj('CourseService', {
+      getTicketFieldInfo: Promise.resolve(''),
+    });
+
+    fakeTicketService = jasmine.createSpyObj('TicketService', {
+      addFaq: undefined,
+      editFaq: undefined,
+      getTicket: undefined
+    });
+
+    // Luodaan komponentti tiketin luomistilassa
+    window.history.pushState({ editFaq: 'false'}, '', '');
+
     await TestBed.configureTestingModule({
-      declarations: [ SubmitFaqComponent ]
+      declarations: [
+        MockComponent(EditAttachmentsComponent),
+        MockComponent(EditorComponent),
+        MockComponent(BeginningButtonComponent),
+        MockComponent(HeadlineComponent),
+        SubmitFaqComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        MatIconModule,
+        MatInputModule,
+        ReactiveFormsModule
+      ],
+      providers: [
+        { provide: CourseService, useValue: fakeCourseService },
+        { provide: TicketService, useValue: fakeTicketService }
+      ]
     })
     .compileComponents();
 
