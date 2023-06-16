795908b96e4b33006d887c950d6544961534fe0b
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-17 01:45:15 +0300

    menu-link: testi 'should create' onnistuu

--- src/app/shared/editor/menu-link/menu-link.component.spec.ts
+++ src/app/shared/editor/menu-link/menu-link.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatIconModule } from '@angular/material/icon';
+import { Editor } from 'ngx-editor';
 
 import { MenuLinkComponent } from './menu-link.component';
 
@@ -8,12 +10,18 @@ describe('MenuLinkComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuLinkComponent ]
+      declarations: [ MenuLinkComponent ],
+      imports: [
+        MatIconModule
+      ]
     })
     .compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(MenuLinkComponent);
     component = fixture.componentInstance;
+    component.editor = new Editor();
     fixture.detectChanges();
   });
 
