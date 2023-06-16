4c87c4dd971e21be7d2cc57d8fc0e104b7228d2d
Erkki Tervo <erkki.tervo@oulu.fi>
2023-06-16 17:11:05 +0300

    refresh-dialog: testi 'should create' onnistuu

--- src/app/core/refresh-dialog/refresh-dialog.component.spec.ts
+++ src/app/core/refresh-dialog/refresh-dialog.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 import { RefreshDialogComponent } from './refresh-dialog.component';
+import { AuthService } from '@core/services/auth.service';
 
 describe('RefreshDialogComponent', () => {
   let component: RefreshDialogComponent;
+  let fakeAuthService: jasmine.SpyObj<AuthService>;
   let fixture: ComponentFixture<RefreshDialogComponent>;
 
   beforeEach(async () => {
+    fakeAuthService = jasmine.createSpyObj('AuthService', {
+      removeDenyConsent: undefined
+    });
+
     await TestBed.configureTestingModule({
-      declarations: [ RefreshDialogComponent ]
+      declarations: [ RefreshDialogComponent ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
 
