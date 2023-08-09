66dec07258ed96fc45d65d21325af1a3e42bc1f1
Henri K <12832691+nkahe@users.noreply.github.com>
2023-08-09 12:44:50 +0300

    add: testi 'handles error when sending invitation'

--- src/app/course/course.service.spec.ts
+++ src/app/course/course.service.spec.ts
@@ -3,6 +3,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { CourseService } from './course.service';
 import { environment } from 'src/environments/environment';
+import { Role } from '@core/core.models';
+import { ErrorService } from '@core/services/error.service';
 
 const courseID = '1';
 const courseName = 'Testikurssi';
@@ -11,6 +13,7 @@ const api = environment.apiBaseUrl;
 describe('CourseService', () => {
   let courses: CourseService;
   let controller: HttpTestingController;
+  let errors: ErrorService
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -18,6 +21,7 @@ describe('CourseService', () => {
     });
     courses = TestBed.inject(CourseService);
     controller = TestBed.inject(HttpTestingController);
+    errors = TestBed.inject(ErrorService);
   });
 
   it('should be created', () => {
@@ -39,4 +43,50 @@ describe('CourseService', () => {
     request.flush({ nimi: 'Testikurssi' });
     controller.verify();
   })
+
+  it('sends invitation successfully', async () => {
+    const courseID = '1';
+    const email = 'test@example.com';
+    const role: Role = 'opiskelija';
+    const expectedResponse = { success: true, kutsu: 'invitationCode' };
+
+    courses.sendInvitation(courseID, email, role).then((response) => {
+      expect(response).toEqual(expectedResponse);
+    });
+
+    const req = controller.expectOne(
+      `${api}/kurssi/${courseID}/osallistujat/kutsu`
+    );
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({ sposti: email, rooli: role });
+
+    req.flush(expectedResponse);
+  });
+
+  it('handles error when sending invitation', (done) => {
+    const courseID = '1';
+    const email = 'test@example.com';
+    const role: Role = 'opiskelija';
+    const errorMessage = 'Some error message';
+
+    courses.sendInvitation(courseID, email, role).then(() => {
+      fail('Ei catchattu.');
+    }).catch((error) => {
+      console.log(error);
+      expect(error).toBeDefined();
+      expect(error.tunnus).toBe(3004);
+      done();
+    });
+
+    const req = controller.expectOne(
+      `${api}/kurssi/${courseID}/osallistujat/kutsu`
+    );
+    expect(req.request.method).toBe('POST');
+
+    const status = 500;
+    const errorid = 3004;
+    const error = errors.createError(status, errorid);
+    req.flush(error, { status: status, statusText: 'Error'});
+  });
+  
 });
