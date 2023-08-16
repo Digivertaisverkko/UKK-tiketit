57004f733fcd115e58ed54449bdbfceadc89d4bb
henri <12832691+nkahe@users.noreply.github.com>
2023-08-16 16:00:35 +0300

    add: course.dummydata.ts testejä varten

---
+++ src/app/course/course.dummydata.ts
@@ -0,0 +1,30 @@
+export const courseDummyData = {
+  fields:  [
+    {
+      id: 1,
+      arvo: "2",
+      otsikko: "Tehtävä",
+      tyyppi: 1,
+      ohje: "",
+      esitaytettava: false,
+      pakollinen: true,
+      valinnat: [
+        ""
+      ]
+    },
+    {
+      id: 2,
+      arvo: "Ongelma",
+      otsikko: "Ongelman tyyppi",
+      tyyppi: 1,
+      ohje: "",
+      esitaytettava: false,
+      pakollinen: true,
+      valinnat: [
+        "Kotitehtävä",
+        "Määräajat",
+        "Yleinen"
+      ]
+    }
+  ]
+}
