5ea2d6f19908ec55b89895093f2f08d438594e19
henri <12832691+nkahe@users.noreply.github.com>
2023-08-22 13:07:28 +0300

    ticket-list classin testejä

--- src/app/ticket/listing/ticket-list/ticket-list.component.spec.ts
+++ src/app/ticket/listing/ticket-list/ticket-list.component.spec.ts
@@ -10,22 +10,31 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockComponent } from 'ng-mocks';
+import { TestScheduler } from 'rxjs/testing';
 
 import { MatTableHarness } from '@angular/material/table/testing';
 
 import { TicketListComponent } from './ticket-list.component';
 import { SearchBarComponent } from '@shared/components/search-bar/search-bar.component';
-import { SortableTicket, TicketService } from '@ticket/ticket.service';
+import { TicketService } from '@ticket/ticket.service';
 import { ticketDummyData } from '@ticket/ticket.dummydata';
 import { DebugElement } from '@angular/core';
 
 describe('TicketListComponent', () => {
   let component: TicketListComponent;
-  let fakeTicketService: Partial<TicketService>;
+
+  let loader: HarnessLoader;
+  let ticketService: Partial<TicketService>;
   // let fakeTicketService: jasmi ne.SpyObj<TicketService>;
   let fixture: ComponentFixture<TicketListComponent>;
   let getTicketListSpy: jasmine.Spy;
-  let loader: HarnessLoader;
+
+  // Alla oleva ei toiminut, jos siirsi beforeEach.
+  ticketService = jasmine.createSpyObj('TicketService', ['getTicketList']);
+  getTicketListSpy = ticketService.getTicketList as jasmine.Spy;
+  getTicketListSpy.and.returnValue(
+    Promise.resolve(ticketDummyData.ticketListClientData)
+  )
 
   beforeEach(async () => {
     /*
@@ -45,13 +54,6 @@ describe('TicketListComponent', () => {
     fakeTicketService = {
       getTicketList: getTicketListSpy
     }; */
-
-    fakeTicketService = jasmine.createSpyObj('TicketService', ['getTicketList']);
-
-    getTicketListSpy = fakeTicketService.getTicketList as jasmine.Spy;
-    getTicketListSpy.and.returnValue(
-      Promise.resolve(ticketDummyData.ticketListClientData)
-    );
 
     // spyOn(fakeTicketService, 'getTicketList').and.callThrough();
 
@@ -75,34 +77,120 @@ describe('TicketListComponent', () => {
         RouterTestingModule
       ],
       providers: [
-        { provide: TicketService, useValue: fakeTicketService }
+        { provide: TicketService, useValue: ticketService }
       ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(TicketListComponent);
     component = fixture.componentInstance;
+
+    // Tällä tulee error: No provider for TicketListComponent!
+    // component = TestBed.inject(TicketListComponent);
+
     loader = TestbedHarnessEnvironment.loader(fixture);
-    fixture.detectChanges();
+     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('ticket list table is created', fakeAsync (() => {
+  describe('fetches data for Mat table dataSource correctly.', () => {
     const courseID = '1';
-    component.fetchTickets(courseID);
 
-    tick();
+    it('fetches correct ticket data for dataSource.', fakeAsync (() => {
+      component.fetchTickets(courseID);
+      tick();
+      const tableDebugElement: DebugElement = fixture.debugElement.query(By.css('.theme-table')); // Update the selector
+      console.dir(component.dataSource);
+      expect(tableDebugElement).toBeTruthy();
 
-    const tableDebugElement: DebugElement = fixture.debugElement.query(By.css('.theme-table')); // Update the selector
-    expect(tableDebugElement).toBeTruthy(); // Check if the table exists
+      expect(component.dataSource.filteredData.length).toBeGreaterThan(0);
+      expect(component.dataSource.filteredData).toEqual(ticketDummyData.ticketListClientData);
+      expect(component.dataSource.data).toEqual(ticketDummyData.ticketListClientData);
 
+      /*
+      const rowsDebugElements: DebugElement[] = tableDebugElement.queryAll(By.css('.mat-row'));
+      expect(rowsDebugElements.length).toBeGreaterThan(0); // Check if there are rows
+      */
+    }));
+
+    it('fetches ticket data for sorting correctly.', fakeAsync (() => {
+      component.fetchTickets(courseID);
+      tick();
+      console.dir(component.sortQuestions);
+      expect(component.dataSource.sort).toBeDefined();
+
+    }));
+
+    it('sets default sorting to "Edited/Muokattu" column starting from ascending order.', fakeAsync (() => {
+      component.fetchTickets(courseID);
+      tick();
+      const sortFn = component.dataSource.sort;
+      expect(sortFn).toBeDefined();
+      expect(sortFn?.active).toBe('viimeisin');
+      expect(sortFn?.start).toBe('asc');
+      const columnCount = 4;
+      expect(sortFn?.sortables.size).toBe(columnCount);
+    }));
+
+    it('sets at least four sorting columns.', fakeAsync (() => {
+      component.fetchTickets(courseID);
+      tick();
+      const sortFn = component.dataSource.sort;
+      expect(sortFn?.sortables.size).toBeGreaterThanOrEqual(4);
+    }));
+
+    it('sets filtering function.', fakeAsync (() => {
+      component.fetchTickets(courseID);
+      tick();
+      const dataSource = component.dataSource;
+      expect(dataSource.filterPredicate).toBeDefined();
+    }));
+
+    // Ei toimi.
     /*
-    const rowsDebugElements: DebugElement[] = tableDebugElement.queryAll(By.css('.mat-row'));
-    expect(rowsDebugElements.length).toBeGreaterThan(0); // Check if there are rows
+    it('fetches tickets 3 times in 2 minutes', fakeAsync(() => {
+
+      const pollingRateMin = 1;
+      const pollingRateSec = pollingRateMin * 60;
+
+      fixture.detectChanges();
+      //component.ngOnInit();
+
+      expect(getTicketListSpy).toHaveBeenCalledTimes(1);
+
+      tick(pollingRateSec * 1000);
+
+      expect(getTicketListSpy).toHaveBeenCalledTimes(2);
+
+      tick(pollingRateSec * 1000);
+
+      expect(getTicketListSpy).toHaveBeenCalledTimes(3);
+    }));
     */
-  }))
+
+    /* Ei toimi.
+    it('sets 5 sortable columns when there is one with attachment', fakeAsync (() => {
+      let dummyData = ticketDummyData.ticketListClientData;
+      dummyData[1].liite = true;
+      getTicketListSpy = ticketService.getTicketList as jasmine.Spy;
+      getTicketListSpy.and.returnValue(
+        Promise.resolve(dummyData)
+      );
+
+      component.fetchTickets(courseID);
+      tick();
+      const sortFn = component.dataSource.sort;
+      expect(sortFn?.sortables.size).toBeGreaterThanOrEqual(5);
+
+      console.warn('spec');
+      console.log(component.dataSource.sort?.sortables);
+    }));
+    */
+
+  });
 
 });
