d7baa40584c9d56fb32b83df3af605dd7b684b46
henri <12832691+nkahe@users.noreply.github.com>
2023-08-23 10:22:44 +0300

    fix: headline -testi: error pois:välitä kurssi ID

--- src/app/shared/components/headline/headline.component.spec.ts
+++ src/app/shared/components/headline/headline.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute, convertToParamMap, ParamMap } from '@angular/router';
+import { of } from 'rxjs';
 
 import { HeadlineComponent } from './headline.component';
 import { CourseService } from '@course/course.service';
@@ -8,6 +10,7 @@ describe('HeadlineComponent', () => {
   let component: HeadlineComponent;
   let fakeCourseService: jasmine.SpyObj<CourseService>;
   let fixture: ComponentFixture<HeadlineComponent>;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async () => {
     fakeCourseService = jasmine.createSpyObj('CourseService', {
@@ -20,13 +23,20 @@ describe('HeadlineComponent', () => {
         RouterTestingModule
       ],
       providers: [
-        { provide: CourseService, useValue: fakeCourseService }
+        { provide: CourseService, useValue: fakeCourseService },
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of(convertToParamMap({ courseid: '1' }))
+          }
+        }
       ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(HeadlineComponent);
     component = fixture.componentInstance;
+    activatedRoute = TestBed.inject(ActivatedRoute);
     fixture.detectChanges();
   });
 
