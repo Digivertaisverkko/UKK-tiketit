e62b56fad9e087c25e52dc95c9639bd037b39d45
henri <12832691+nkahe@users.noreply.github.com>
2023-09-12 10:01:51 +0300

    remove: käyttämättömäksi jäänyt auth.guard.
    ticket routing module siivousta.

--- src/app/ticket/ticket-routing.module.ts
+++ src/app/ticket/ticket-routing.module.ts
@@ -1,35 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { authGuard } from '../user/auth.guard';
 
-import { TicketViewComponent } from './ticket-view/ticket-view.component';
-import { SubmitTicketComponent } from './submit-ticket/submit-ticket.component';
-import { SubmitFaqComponent } from './submit-faq/submit-faq.component';
 import { FaqViewComponent } from './faq-view/faq-view.component';
 import { ListingComponent } from './listing/listing.component';
+import { SubmitFaqComponent } from './submit-faq/submit-faq.component';
+import { SubmitTicketComponent } from './submit-ticket/submit-ticket.component';
+import { TicketViewComponent } from './ticket-view/ticket-view.component';
 
 const routes: Routes = [
-  { path: 'course/:courseid/ticket-view/:id',
-      component: TicketViewComponent },
-      // canActivate: [authGuard] },
+  { path: 'course/:courseid/ticket-view/:id', component: TicketViewComponent },
 
-  { path: 'course/:courseid/submit',
-      component: SubmitTicketComponent },
+  { path: 'course/:courseid/submit', component: SubmitTicketComponent },
 
-  { path: 'course/:courseid/submit/:id',
-      component: SubmitTicketComponent },
+  { path: 'course/:courseid/submit/:id', component: SubmitTicketComponent },
 
-  { path: 'course/:courseid/submit-faq',
-      component: SubmitFaqComponent },
+  { path: 'course/:courseid/submit-faq', component: SubmitFaqComponent },
 
-  { path: 'course/:courseid/submit-faq/:id',
-      component: SubmitFaqComponent },
+  { path: 'course/:courseid/submit-faq/:id', component: SubmitFaqComponent },
 
-  { path: 'course/:courseid/faq-view/:id',
-      component: FaqViewComponent },
+  { path: 'course/:courseid/faq-view/:id', component: FaqViewComponent },
 
-  { path: 'course/:courseid/list-tickets',
-      component: ListingComponent },
+  { path: 'course/:courseid/list-tickets', component: ListingComponent },
 ];
 //   { path: 'list-tickets', component: ListingComponent }
 
