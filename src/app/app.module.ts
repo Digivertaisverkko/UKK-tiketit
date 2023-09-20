a89a74b244ce82e07834591477c7c7c884920e81
henri <12832691+nkahe@users.noreply.github.com>
2023-09-20 15:47:42 +0300

    ticketListComponent app.module -> ticket.module
    +kommentointeja ja siivoilua.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,37 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-// import { initializeSupportedLocales } from './app.initializers';
-import { LOCALE_ID } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID } from '@angular/core';
 
+import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { CourseModule } from './course/course.module';
+import { initializeLanguage  } from "./app.initializers";
 import { SharedModule } from './shared/shared.module';
 import { TicketModule } from './ticket/ticket.module';
 import { UserModule } from './user/user.module';
-import { CourseModule } from './course/course.module';
 
-import { AppComponent } from './app.component';
-import { ListingComponent } from './ticket/listing/listing.component';
-import { initializeLanguage  } from "./app.initializers";
-
-// AppRoutingModule pitää tulla viimeisimpänä ennen muita routingeja sisältäviä
-// moduuleja. Oletuksena käytetään aina fi-FI -localea.
+/**
+ * Täällä importoidaan kaikki päämoduulit. Tämän moduulin ei tulisi sisältää
+ * muita komponentteja tai servicejä, vaan niiden tulisi olla muissa moduuleissa.
+ * Core.Modulen import pitäisi olla vain tässä tiedostossa.
+ *
+ * ! AppRoutingModule pitää olla imports-taulukossa viimeisimpänä.
+ *
+ * @export
+ * @class AppModule
+ */
 @NgModule({
   declarations: [
-    AppComponent,
-    ListingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     CoreModule,
+    CourseModule,
     SharedModule,
     TicketModule,
     UserModule,
-    CourseModule,
     AppRoutingModule,
   ],
   providers: [
@@ -38,11 +42,4 @@ import { initializeLanguage  } from "./app.initializers";
   exports: []
 })
 
-
 export class AppModule { }
-
-// useFactory: initializeSupportedLocales
-// { provide: LOCALE_ID, useValue: 'fi' }
-
-// { provide: LOCALE_ID, useFactory: () => initializeLocale, multi: true },
-//  { provide: LOCALE_ID, useFactory: initializeSupportedLocales }
--- src/app/ticket/ticket.module.ts
+++ src/app/ticket/ticket.module.ts
@@ -1,40 +1,40 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SharedModule } from '../shared/shared.module';
-
-import { TicketRoutingModule } from './ticket-routing.module';
-import { TicketViewComponent } from './ticket-view/ticket-view.component';
-import { SubmitTicketComponent } from './submit-ticket/submit-ticket.component';
-import { MatSortModule } from '@angular/material/sort';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSortModule } from '@angular/material/sort';
+import { NgModule } from '@angular/core';
+import { TicketRoutingModule } from './ticket-routing.module';
+import { TicketViewComponent } from './ticket-view/ticket-view.component';
 
-// import { MatInputModule } from '@angular/material/input';
-// import { MatFormFieldModule } from '@angular/material/form-field';
-// MatFormFieldModule,
-// MatInputModule,
-
-import { FaqViewComponent } from './faq-view/faq-view.component';
-import { MessageComponent } from './components/message/message.component';
-import { SubmitFaqComponent } from './submit-faq/submit-faq.component';
-import { ViewAttachmentsComponent } from './components/view-attachments/view-attachments.component';
-import { EditAttachmentsComponent } from './components/edit-attachments/edit-attachments.component';
 import { CommentComponent } from './components/comment/comment.component';
+import { EditAttachmentsComponent } from './components/edit-attachments/edit-attachments.component';
+import { FaqViewComponent } from './faq-view/faq-view.component';
+import { ListingComponent } from './listing/listing.component';
+import { MessageComponent } from './components/message/message.component';
+import { SharedModule } from '../shared/shared.module';
+import { SubmitFaqComponent } from './submit-faq/submit-faq.component';
+import { SubmitTicketComponent } from './submit-ticket/submit-ticket.component';
 import { TicketListComponent } from './listing/ticket-list/ticket-list.component';
+import { ViewAttachmentsComponent } from './components/view-attachments/view-attachments.component';
 
-
+/**
+ * Tiketteihin eli kysymyksiin liittyvä toiminnallisuus.
+ *
+ * @export
+ * @class TicketModule
+ */
 @NgModule({
   declarations: [
+    CommentComponent,
     EditAttachmentsComponent,
     FaqViewComponent,
+    ListingComponent,
     MessageComponent,
     SubmitFaqComponent,
     SubmitTicketComponent,
+    TicketListComponent,
     TicketViewComponent,
     ViewAttachmentsComponent,
-    CommentComponent,
-    TicketListComponent,
   ],
   imports: [
     CommonModule,
@@ -43,9 +43,6 @@ import { TicketListComponent } from './listing/ticket-list/ticket-list.component
     MatSortModule,
     SharedModule,
     TicketRoutingModule,
-  ],
-  exports: [
-    TicketListComponent
   ]
 })
 
